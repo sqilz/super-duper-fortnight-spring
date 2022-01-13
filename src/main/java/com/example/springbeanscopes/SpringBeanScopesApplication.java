46225792bf33b65bab0c208b59d7eb775556da46
Przemyslaw Hendel <przemyslaw.hendel@contractors.roche.com>
2022-01-13 08:20:46 +0100

    initial commit

---
+++ src/main/java/com/example/springbeanscopes/SpringBeanScopesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbeanscopes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeanScopesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBeanScopesApplication.class, args);
+    }
+
+}
